28999f5d9437478d91f5be12a85fb506af192d0d
sisrfeng <liuweifengemail@163.com>
2020-11-10 19:02:22 +0800

    wf_OK

--- auto_install.sh
+++ auto_install.sh
@@ -22,18 +22,23 @@ EOF
 
 yes | (apt-get update ; apt-get upgrade ; apt-get install zsh)
 yes | (cp ~/dot_file/.zshrc ~/ && source ~/.zshrc )
-zsh
-yes | (apt install libevent-dev ; apt install libevent-dev ; apt-get install aptitude ;aptitude update ; ai sudo; ai git ; ai tmux ;ai vim-gtk; ai ack ; ai exiftool;  ai htop; ai tree ; ai tzdata ; chsh -s `which zsh`; ai locales ; ai language-pack-zh-hans-baseai language-pack-zh-hans ; ai curl ; apt install peco ; pip install --upgrade pip ; pip install pudb ; pip install gpustat ; pip install tldr ; sh -c "$(curl -fSL https://raw.githubusercontent.com/hoseahsu/oh-my-tmux/master/install.sh)" ; curl -sLf https://spacevim.org/cn/install.sh | zsh ; apt install python3-dev python3-pip python3-setuptools ; pip3 install thefuck)
+yes | (apt install libevent-dev ; apt install libevent-dev ; apt-get install aptitude ;aptitude update ; ai sudo; ai git ; ai tmux ;ai vim-gtk; ai ack ; ai exiftool;  ai htop; ai tree ; ai tzdata ; chsh -s `which zsh`; ai locales ; ai language-pack-zh-hans-baseai language-pack-zh-hans ; ai curl ; apt install peco ; pip install --upgrade pip ; pip install pudb ; pip install gpustat ; pip install tldr ; sh -c "$(curl -fSL https://raw.githubusercontent.com/hoseahsu/oh-my-tmux/master/install.sh)" ; curl -sLf https://spacevim.org/cn/install.sh | bash ; apt install python3-dev python3-pip python3-setuptools ; pip3 install thefuck)
 
+yes | (apt-get install language-pack-zh-hans language-pack-zh-hans-base language-pack-gnome-zh-hans language-pack-gnome-zh-hans-base)
+
+
+locale-gen zh_CN.UTF-8
 
 #Linux日期不准确，要更改 Linux 系统整个系统范围的时区可以使用如下命令：
 sudo rm -f /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-# 修改默认python
-sudo rm /usr/bin/python
-sudo ln -s /usr/bin/python3.? /usr/bin/python
 
 
 yes | (cp ~/dot_file/vimrc ~/.SpaceVim/vimrc ; cp ~/dot_file/.tmux.conf  ~/.tmux.conf ; cp ~/dot_file/init.toml  ~/.SpaceVim.d/init.toml; cp ~/dot_file/.zshrc ~/ && source ~/.zshrc )
 
-#一键执行的命令结束。
+
+# 修改默认python
+sudo rm /usr/bin/python
+sudo ln -s /usr/bin/python3.? /usr/bin/python
+
+zsh
