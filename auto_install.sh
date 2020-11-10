7960a1b3b5b91fbc3e79ebfd61596e2f72afbebb
sisrfeng <liuweifengemail@163.com>
2020-11-10 19:12:52 +0800

    wf_OK

--- auto_install.sh
+++ auto_install.sh
@@ -28,13 +28,8 @@ yes | (apt install libevent-dev ; apt install libevent-dev ; apt-get install apt
 yes | (pip install --upgrade pip ; apt-get install language-pack-zh-hans language-pack-zh-hans-base language-pack-gnome-zh-hans language-pack-gnome-zh-hans-base)
 
 
-locale-gen zh_CN.UTF-8
-
 #Linux日期不准确，要更改 Linux 系统整个系统范围的时区可以使用如下命令：
-sudo rm -f /etc/localtime
-sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
-
+locale-gen zh_CN.UTF-8 ; sudo rm -f /etc/localtime && sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 yes | (cp ~/dot_file/vimrc ~/.SpaceVim/vimrc ; cp ~/dot_file/.tmux.conf  ~/.tmux.conf ; cp ~/dot_file/init.toml  ~/.SpaceVim.d/init.toml; cp ~/dot_file/.zshrc ~/ && source ~/.zshrc )
 
 
