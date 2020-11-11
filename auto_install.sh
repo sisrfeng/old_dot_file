c8c0a20f6e7b5b0e7b64f3cc5a3afe6ff2baf8d0
root <root@interactive75595.2080ti.svc.kube.local>
2020-11-11 13:47:18 +0800

    aa

--- auto_install.sh
+++ auto_install.sh
@@ -36,16 +36,14 @@ unset all_proxy
 unset ALL_PROXY
 
 yes | (apt-get update ; apt-get upgrade ; apt-get install zsh)
-yes | (apt install libevent-dev ; apt install libevent-dev ; apt-get install aptitude ;aptitude update ; aptitude install sudo python-pip;pip install pysocks; aptitude install git ; aptitude install tmux ;aptitude install vim-gtk; aptitude install ack ; aptitude install exiftool;  aptitude install htop; aptitude install tree ; aptitude install tzdata ; chsh -s `which zsh`; aptitude install locales ; aptitude install language-pack-zh-hans language-pack-zh-hans-base; aptitude install curl; pip install --upgrade pip ; pip install pudb ; pip install gpustat ; pip install tldr ; sh -c "$(curl -fSL https://raw.githubusercontent.com/hoseahsu/oh-my-tmux/master/install.sh)" ; curl -sLf https://spacevim.org/cn/install.sh | bash ; apt install python3-dev python3-pip python3-setuptools ; pip3 install thefuck)
+yes | (apt install libevent-dev ; apt install libevent-dev ; apt-get install aptitude ;aptitude update ; aptitude install sudo python3-pip;pip3 install pysocks; aptitude install git ; aptitude install tmux ;aptitude install vim-gtk; aptitude install ack ; aptitude install exiftool;  aptitude install htop; aptitude install tree ; aptitude install tzdata ; chsh -s `which zsh`; aptitude install locales ; aptitude install language-pack-zh-hans language-pack-zh-hans-base; aptitude install curl; pip3 install --upgrade pip ; pip3 install pudb ; pip3 install gpustat ; pip3 install tldr ; sh -c "$(curl -fSL https://raw.githubusercontent.com/hoseahsu/oh-my-tmux/master/install.sh)" ; curl -sLf https://spacevim.org/cn/install.sh | bash ; apt install python3-dev python3-pip python3-setuptools ; pip3 install thefuck)
 
 
-yes | (pip install --upgrade pip ; apt-get install language-pack-zh-hans language-pack-zh-hans-base language-pack-gnome-zh-hans language-pack-gnome-zh-hans-base ; aptitude install peco)
-
+yes | (apt-get install language-pack-zh-hans language-pack-zh-hans-base ; aptitude install peco)
 
 #Linux日期不准确，要更改 Linux 系统整个系统范围的时区可以使用如下命令：
 locale-gen zh_CN.UTF-8 ; sudo rm -f /etc/localtime && sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 yes | (cp ~/dot_file/vimrc ~/.SpaceVim/vimrc ; cp ~/dot_file/.tmux.conf  ~/.tmux.conf ; cp ~/dot_file/init.toml  ~/.SpaceVim.d/init.toml; cp ~/dot_file/.zshrc ~/ && source ~/.zshrc )
-
 
 # 修改默认python
 sudo rm /usr/bin/python
