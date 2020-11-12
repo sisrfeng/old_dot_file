7168c1c7f5ab363264fa51ed2a04dccf576ac92f
root <root@interactive42382.2080ti.svc.kube.local>
2020-11-12 15:29:11 +0800

    ok

--- auto_install.sh
+++ auto_install.sh
@@ -45,6 +45,14 @@ yes | (apt-get install language-pack-zh-hans language-pack-zh-hans-base ; aptitu
 #Linux日期不准确，要更改 Linux 系统整个系统范围的时区可以使用如下命令：
 locale-gen zh_CN.UTF-8 ; sudo rm -f /etc/localtime && sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime ; yes | (cp ~/dot_file/vimrc ~/.SpaceVim/vimrc ; cp ~/dot_file/.tmux.conf  ~/.tmux.conf ; cp ~/dot_file/init.toml  ~/.SpaceVim.d/init.toml; cp ~/dot_file/.zshrc ~/ && source ~/.zshrc )
 
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally
+mv squashfs-root / && ln -s /squashfs-root/AppRun /usr/bin/vnvim
 
 # 修改默认python
 sudo rm /usr/bin/python  ; sudo ln -s /usr/bin/python3.? /usr/bin/python ; zsh
