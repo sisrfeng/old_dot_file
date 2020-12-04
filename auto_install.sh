1a41ccc7db83f6319318d1906903254e8df211c0
sisrfeng <liuweifengemail@163.com>
2020-12-04 23:08:16 +0800

    wf

--- auto_install.sh
+++ auto_install.sh
@@ -22,10 +22,6 @@ yes | (ln -s /opt/data/private/anaconda3 ~/)
 yes | (add-apt-repository ppa:ultradvorka/ppa && apt-get -qq update && apt-get -qq upgrade)
 yes | (apt install aptitude ;aptitude update -q; ai sudo python3-pip hstr zsh progress libevent-dev)
 
-unset ALL_PROXY
-pip install --upgrade pip
-pip3 install --upgrade pip
-pip3 install pysocks
 yes | (ai htop ; ai tmux ;ai vim-gtk; ai ack)
 yes | (ai exiftool htop tree zdata locales language-pack-zh-hans language-pack-zh-hans-base)
 yes | (ai curl; pip3 install --upgrade pip ; pip3 install pudb ; pip3 install gpustat ; pip3 install tldr )
